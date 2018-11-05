c62bb7944ec97408a66e68d50ac293c87531f894
DrDrako <44775454+DrDrako@users.noreply.github.com>
2018-11-05 09:45:07 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+  function changeBG(el,clr) {
+   var elem = document.getElementById(el);
+   elem.style.transition ="color 1.0s linear 0s";
+   elem.style.color = clr;
+  }
+  
+  window.onload = function (){
+      
+  }
